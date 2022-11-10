06edd72e8f976d3aa1a22dedb7d1a4e48c52e3e1
jegstar <wjeggo@hotmail.com>
2022-11-10 15:21:12 +0800

    got timer working with text input

--- binaryCountdown/countdown.js
+++ binaryCountdown/countdown.js
@@ -2,11 +2,13 @@ let timer=0;
 let intervalId;
 
 const startCountdown = (time) => {
-    timer = time
-    update(time)
-    if (!intervalId) {
-        intervalId = setInterval(update, 1000, time)
+    timer = time - 1    
+    showBinary(time)
+    if (intervalId)
+    {
+        clearInterval(intervalId)
     }
+    intervalId = setInterval(update, 1000, timer)
 }
 
 const update = (time) => {
@@ -16,7 +18,6 @@ const update = (time) => {
         resetBlocks()
     }
     else {
-
         showBinary(timer)
         timer = timer - 1
     }
@@ -24,7 +25,6 @@ const update = (time) => {
 
 const resetBlocks = () => {
     const items = document.getElementsByClassName("binary-block")
-    console.log(items)
     Array.prototype.forEach.call(items, (element => {
         element.classList = "inactive binary-block"
     }));
@@ -35,7 +35,10 @@ const showBinary = (seconds) => {
     for (let i = 0; i < binString.length; i ++) {
         const item = document.getElementById(i);
         // item.innerText = binString.charAt(i);
-        item.classList.remove('inactive')
+        if (item.classList.contains('inactive'))
+        {
+            item.classList.remove('inactive')
+        }
         if (binString.charAt(i) == 1)
         {
             item.classList.remove("off")
@@ -64,7 +67,6 @@ const createDivs = () => {
         const item = document.createElement('div')
         item.classList.add("binary-block")
         item.id = i
-        //item.innerText = i
         grid.appendChild(item)
         divs.push(item)
     }
@@ -78,8 +80,10 @@ const onLoad = () => {
     resetBlocks();
     const startTimer = document.getElementById("start-timer");
     startTimer.addEventListener("click", () => {
-        const seconds = document.getElementById('seconds').value
-        startCountdown(seconds)
+        const seconds = parseInt(document.getElementById('seconds').value)
+        const minutes = parseInt(document.getElementById('minutes').value)
+        console.log(minutes * 60 + seconds)
+        startCountdown(minutes * 60 + seconds)
     })
 }
 
